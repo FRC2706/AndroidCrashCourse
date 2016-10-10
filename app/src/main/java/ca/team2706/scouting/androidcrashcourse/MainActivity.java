5ad5dc61181bb6ccdbf0c0d60db95c986ce32165
Mike Ounsworth <ounsworth@gmail.com>
2016-10-10 11:23:27 -0400

    Initial commit; blank Android project

---
+++ app/src/main/java/ca/team2706/scouting/androidcrashcourse/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.team2706.scouting.androidcrashcourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
