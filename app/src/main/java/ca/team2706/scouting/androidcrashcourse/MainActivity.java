50a9c2cb85be36b19564ba63a88ee695554106ca
Matthew Tzavelas <Omnilockguy@gmail.com>
2016-11-16 20:27:18 -0500

    My changes

--- app/src/main/java/ca/team2706/scouting/androidcrashcourse/MainActivity.java
+++ app/src/main/java/ca/team2706/scouting/androidcrashcourse/MainActivity.java
@@ -1,13 +1,35 @@
 package ca.team2706.scouting.androidcrashcourse;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    String password = "rekt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onButtonClick(v);
+            }
+        });
+
+    }
+
+    public void onButtonClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String value = editText.getText().toString();
+        if (value.equals(password)) {
+            editText.setText ("Correct!");
+        }
     }
 }
